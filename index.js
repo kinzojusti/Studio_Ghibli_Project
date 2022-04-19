8bfbfa326a14b3bbe36dd28f9dfd0f0099de9599
+ <+>
2022-04-19 12:24:54 -0400

    added fetch data-NWY!

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+    // const localHost ='https://localhost:3000';
+    // const getData = () => {
+    // fetch('https://localHost' + '/films')
+    //     .then(resp => resp.json())
+    //     .then(arrOfMovies => displayArrOfMovies(arrOfMovies))
+    // } 
 
-    const localHost ='https://localhost:3000';
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const getUrl = ('https://ghibliapi.herokuapp.com/films');
+        
+        function fetchMovies(){
+            fetch(getUrl)
+            .then(response => response.json())
+            .then(console.log)
+        }
+
+
+        function renderMovies(moviesArray){
+            console.log(moviesArray)
+            const heroList = document.getElementById('hero-list')
+            moviesArray.forEach(movie => {
+            
+                const movieLi = document.createElement('li')
+                movieLi.textContent = movie.name
+                const movieImg = document.createElement('img')
+                movieImg.src = movie.image
+                const movieTitle = document.createElement('p')
+                movieTitle.textContent = movie.title
     
-    const getData = () => {
-    fetch('https://localHost' + '/films')
-        .then(resp => resp.json())
-        .then(arrOfMovies => displayArrOfMovies(arrOfMovies))
-    } 
-    
+                movieLi.append()
+                movieImg.append()
+                movieTitle.append()
+                
+            })
+        }
+        renderMovies()
+})
