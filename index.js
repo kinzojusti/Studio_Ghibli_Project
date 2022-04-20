9640080ec10af6c18d3e9c979c6ae37761ac44c9
+ <+>
2022-04-19 22:19:09 -0400

    updated with title and images

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-// console.log("Hello World!")
-    // const localHost ='https://localhost:3000';
-    // const getData = () => {
-    // fetch('https://localHost' + '/films')
-    //     .then(resp => resp.json())
-    //     .then(arrOfMovies => displayArrOfMovies(arrOfMovies))
-    // } 
-
-
     document.addEventListener("DOMContentLoaded", function() {
 
+        //below is the Fetch for the data://
         const getUrl = ('https://ghibliapi.herokuapp.com/films');
         
         function fetchMovies(){
@@ -16,24 +8,71 @@
             .then(response => response.json())
             .then(moviesObj => renderMovies(moviesObj))
         }
+        //above is the Fetch:end of fetch//
 
-            //rendering function:
+
+        //below is the rendering function://
         function renderMovies(moviesObj){
-            // console.log(moviesArray)
-            // const movieList = document.getElementById('-list')
-            moviesObj.forEach(movie => renderOneMovie(movie))} 
+            moviesObj.forEach(movie => render1stMovie(movie))
+            moviesObj.forEach(movie => render2ndMovie(movie))
+            moviesObj.forEach(movie => render3rdMovie(movie))
+        } 
 
-            function renderOneMovie(){
-                const movieLi = document.createElement('#poster-display')
-                movieLi.textContent = movie.name
-                const movieImg = document.createElement('img')
-                movieImg.src = movie.image
-                const movieTitle = document.createElement('p')
-                movieTitle.textContent = movie.title
-                movieLi.append()
-                movieImg.append()
-                movieTitle.append()
+
+        
+        //callbacks below://
+
+            function render1stMovie(movie){
+                console.log(movie)
+                const movieImgTag = document.querySelector('.poster-image')
+                movieImgTag.src = movie.image 
+                const englishTitle = document.querySelector('.engName')
+                englishTitle.textContent = movie.title
+                console.log(englishTitle, "someword")
+                // const movieLi = document.createElement('#poster-display')
+                // movieLi.textContent = movie.name
+                // const movieImg = document.createElement('img')
+                // movieImg.src = movie.image
+                // const movieTitle = document.createElement('p')
+                // movieTitle.textContent = movie.title
+                // movieLi.append()
+                // movieImg.append()
+                // movieTitle.append()
                 
             }
-        })
-        fetchMovies();
+            
+            function render2ndMovie(movie){
+                console.log(movie)
+                const movieImgTag2= document.querySelector('.poster-image2')
+                movieImgTag2.src = movie.image
+                
+                // const movieLi = document.createElement('#poster-display')
+                // movieLi.textContent = movie.name
+                // const movieImg = document.createElement('img')
+                // movieImg.src = movie.image
+                // const movieTitle = document.createElement('p')
+                // movieTitle.textContent = movie.title
+                // movieLi.append()
+                // movieImg.append()
+                // movieTitle.append()
+                
+            }
+            
+            function render3rdMovie(movie){
+                console.log(movie)
+                const movieImgTag3= document.querySelector('.poster-image3')
+                movieImgTag3.src = movie.image
+                
+                // const movieLi = document.createElement('#poster-display')
+                // movieLi.textContent = movie.name
+                // const movieImg = document.createElement('img')
+                // movieImg.src = movie.image
+                // const movieTitle = document.createElement('p')
+                // movieTitle.textContent = movie.title
+                // movieLi.append()
+                // movieImg.append()
+                // movieTitle.append()
+                
+            }
+        fetchMovies()
+    })
